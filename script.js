d4efdb4cad4eb73db463ebcb29bbd007d593db30
Fiona Y <fyeung21@gmail.com>
2022-03-09 14:19:46 -0800

    Connect html elements to JS Object

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const oscarGame = {
+    isRunning: false,
+    splashScreen: $('#splash-screen'),
+    gameScreen: $(''),
+    gameoverScreen: $('#gameover-screen'),
+    nameInput: $('#name-input'),
+    addNameBtn: $('#add-name'),
+    playerDisplay: $('#player-display'),
+    playerName: $('#player-name'),
+    score: $('#score'),
+    playBtn: $('#play-game'),
+    startBtn:$('#start-game'),
+    pauseBtn: $(''),
+    patch: $('patch'),
+    cat: $('cat'),
+    restartBtn: $('#reset-game'),
+    
+
+    
+}
