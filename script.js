b7b050c0a38d93300a8a3d0e45543a56c21a474e
Fiona Y <fyeung21@gmail.com>
2022-11-09 17:51:17 -0800

    Add comments

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const reactionGame = {
     playerName: "",
     setup: () => {
 
+        // Click event checks if player name input is empty. If not empty, proceed to game screen.
         reactionGame.playBtn.on('click', event => {
             if (reactionGame.playerName !== "") {
                 reactionGame.switchScreen("play-game");
@@ -31,6 +32,7 @@ const reactionGame = {
             }
         });
 
+        // Click event checks if pause button is pressed which will then pause game and show/hide appropriate buttons
         reactionGame.pauseBtn.on('click', event => {
             if (reactionGame.isRunning == true) {
                 reactionGame.isRunning = false;
@@ -40,6 +42,7 @@ const reactionGame = {
             }
         });
 
+        // Click event starts game
         reactionGame.startBtn.on('click', event => {
             reactionGame.isRunning = true;
             reactionGame.peep();
@@ -47,6 +50,7 @@ const reactionGame = {
             reactionGame.pauseBtn.show();
         });
 
+        // Click event resets game and game elements
         reactionGame.resetBtn.on('click', event => {
             reactionGame.switchScreen('splash-screen');
             reactionGame.pauseBtn.hide();
@@ -62,6 +66,7 @@ const reactionGame = {
             reactionGame.updateEscapedOscarsDisplay();
         });
 
+        // Click event adds/updates name to screen if name input is not empty
         reactionGame.addNameBtn.on('click', event => {
             if (reactionGame.nameInput.val().length > 0) {
                 reactionGame.message.remove();
@@ -71,8 +76,10 @@ const reactionGame = {
             reactionGame.nameInput.val("");
         });
 
+        // Click event on a hole during the game
         reactionGame.hole.on('click', event => {
 
+            // If Oscar is present on hole, add points, update display, and remove oscar and cats
             if ($(event.currentTarget).children().first().hasClass('oscar')) {
                 reactionGame.soundHit();
                 reactionGame.score++
@@ -84,6 +91,7 @@ const reactionGame = {
                 $('.cat').remove();
                 reactionGame.soundMiss();
 
+                // Score cannot be negative, even if miss while score is 0
                 if (reactionGame.score === 0) {
                     reactionGame.score
 
@@ -95,6 +103,8 @@ const reactionGame = {
 
         });
     },
+
+    // Screen Switching function
     switchScreen: (currentScreen) => {
         if (currentScreen === "play-game") {
             reactionGame.splashScreen.hide();
@@ -121,6 +131,7 @@ const reactionGame = {
         const missAudio = new Audio('./media/sounds/angry.mp3');
         missAudio.play();
     },
+    // Add name on Screen if name input is not empty
     addName: () => {
         if (reactionGame.nameInput.val().length > 0) {
             reactionGame.playerName = reactionGame.nameInput.val();
@@ -128,20 +139,27 @@ const reactionGame = {
             reactionGame.message.text('Please input a name to proceed.');
         }
     },
+    // Update name on Screen
     updatePlayerName: () => {
         reactionGame.playerNameDisplay.text(reactionGame.playerName);
     },
+    // Update score on Screen
     updateScoreDisplay: () => {
         reactionGame.scoreDisplay.text(reactionGame.score);
     },
+    // Update escaped Oscars count on Screen
     updateEscapedOscarsDisplay: () => {
         reactionGame.escapedOscarsDisplay.text(reactionGame.escapedOscars);
     },
     lastHole: $(),
+
+    // Randomize hole and cat functionality
     randomHole: () => {
+        // Randomize index of hole to select random hole
         const index = Math.floor(Math.random() * reactionGame.hole.length);
         const hole = reactionGame.hole[index];
 
+        // If src = oscar.png is in array then add to escapedOscars count
         if ($('[src$="oscar.png"]').length) {
             reactionGame.escapedOscars++
             reactionGame.updateEscapedOscarsDisplay();
@@ -153,19 +171,25 @@ const reactionGame = {
             }
         }
 
+        // Call to peep function (a timeout function written below)
         reactionGame.peep();
 
+        // Check if latest random hole is already current hole
         if (hole === reactionGame.lastHole) {
+            // Restart randomize
             reactionGame.randomHole(hole)
 
         } else if (reactionGame.lastHole != hole) {
+            // Make latest random hole into current hole and remove active class and cat img
             reactionGame.lastHole = hole
             reactionGame.hole.removeClass('active')
             $('.cat').remove()
 
+            // Pick random cat from imgSrc array
             const randCat = reactionGame.imgSrc[Math.floor(Math.random() * reactionGame.imgSrc.length)];
             let img = `<img class='cat ${randCat.split('.')[0]}' src='./media/images/${randCat}' alt='${randCat}'/>`;
 
+            // Add random cat img to hole
             $(hole).addClass('active')
             $(hole).html(img)
 
@@ -177,6 +201,8 @@ const reactionGame = {
     escapedOscars: 0,
     round: 0,
     timeoutId: null,
+    
+    // Peep timeout functionality
     peep: () => {
 
         if (reactionGame.isRunning == true) {
@@ -197,6 +223,8 @@ const reactionGame = {
             } else if (reactionGame.round === 31) {
                 reactionGame.isRunning = false;
                 reactionGame.winScreen.show();
+                reactionGame.updateEscapedOscarsDisplay();
+                reactionGame.updateScoreDisplay();
                 reactionGame.gameScreen.hide();
 
             } else {
